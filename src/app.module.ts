233beff041a7bba97686291ec16202ebefe570c4
User <carlosquispe9876@gmail.com>
2024-02-27 21:42:37 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { RolesModule } from './roles/roles.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'monorail.proxy.rlwy.net',
+      port: 54280,
+      username: 'root',
+      password: 'DAHffcgCHaDcHhafBA21fdh2HeG-aGB-',
+      database: 'railway',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule,
+    RolesModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
